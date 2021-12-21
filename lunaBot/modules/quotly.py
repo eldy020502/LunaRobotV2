1469f8fc0e707e2d0de6b65f0df9347e3cbe5b9e
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2021-12-22 00:31:34 +0800

    Update quotly.py

--- lunaBot/modules/quotly.py
+++ lunaBot/modules/quotly.py
@@ -1,21 +1,3 @@
-# Copyright (C) 2021 TeamDaisyX
-
-
-# This file is part of Daisy (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import json
 import os
 import random
@@ -27,7 +9,7 @@ from fontTools.ttLib import TTFont
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import functions, types
 
-from JisooX.events import register
+from lunabot.events import register
 
 COLORS = [
     "#F07975",
