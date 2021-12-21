1d4e921ceeee44d38a3320ff37277dde0254e62b
eldy020502 <96163277+eldy020502@users.noreply.github.com>
2021-12-22 01:02:20 +0800

    Update quotly.py

--- lunaBot/modules/quotly.py
+++ lunaBot/modules/quotly.py
@@ -9,7 +9,7 @@ from fontTools.ttLib import TTFont
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 from telethon.tl import functions, types
 
-from lunabot.events import register
+from lunaBot.events import register
 
 COLORS = [
     "#F07975",
